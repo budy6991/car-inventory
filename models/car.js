52f9e0468d7b4a229f7bc186089781921e582881
budy6991 <alvmenfer@gmail.com>
2022-12-13 13:56:43 +0100

    Added car Schema

---
+++ models/car.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CarSchema = new Schema({
+  name: { type: String, required: true, maxLenght: 120 },
+  model: { type: String, required: true, maxLenght: 120 },
+  year: { type: Number, min: 1900, max: 2100, required: true },
+  description: { type: String, required: true },
+  manufacturer: { type: Schema.Types.ObjectId, ref: "Manufacturer" },
+  carBody: { type: Schema.Types.ObjectId, ref: "CarBody" },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand" },
+});
