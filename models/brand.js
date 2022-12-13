f2e65dc2490d3a9676318c9abae86b7e33667dbc
budy6991 <alvmenfer@gmail.com>
2022-12-13 14:21:55 +0100

    Added BrandSchema with url virtual prop

---
+++ models/brand.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+BranchSchema.virtual("url").get(function () {
+  return `/catalog/brand/${this._id}`;
+});
+
+module.exports = mongoose.model("Brand", BrandSchema);
