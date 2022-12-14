c2e117e7a078f5c4a4e5e8ec2214454e7b22b805
budy6991 <alvmenfer@gmail.com>
2022-12-14 14:13:32 +0100

    Added some cars

--- populatedb.js
+++ populatedb.js
@@ -146,7 +146,7 @@ function createCarManufacturerBodyBrand(cb) {
       function (callback) {
         manufacturerCreate(
           "Stellantis",
-          "Stellantis N.V. is a multinational automotive manufacturing corporation formed in 2021 on the basis of a 50–50 cross-border merger between the Italian-American conglomerate Fiat Chrysler Automobiles and the French PSA Group. The company is headquartered in Amsterdam",
+          "Stellantis N.V. is a multinational automotive manufacturing corporation formed in 2021 on the basis of a 50-50 cross-border merger between the Italian-American conglomerate Fiat Chrysler Automobiles and the French PSA Group. The company is headquartered in Amsterdam",
           "Amsterdam, The Netherlands",
           callback
         );
@@ -328,13 +328,6 @@ function createCarManufacturerBodyBrand(cb) {
       },
       function (callback) {
         brandCreate(
-          "Kia",
-          "Kia Corporation is a South Korean multinational automobile manufacturer headquartered in Seoul, South Korea. It is South Korea's second largest automobile manufacturer, after its parent company, Hyundai Motor Company, with sales of over 2.8 million vehicles in 2019.",
-          callback
-        );
-      },
-      function (callback) {
-        brandCreate(
           "Hyundai",
           "Hyundai Automobiles is one of the main brands of Hyundai Motor Company.",
           callback
@@ -412,13 +405,6 @@ function createCarManufacturerBodyBrand(cb) {
       },
       function (callback) {
         brandCreate(
-          "Porsche",
-          "Dr. Ing. h.c. F. Porsche AG, usually shortened to Porsche, is a German automobile manufacturer specializing in high-performance sports cars, SUVs and sedans, headquartered in Stuttgart, Baden-Württemberg, Germany.",
-          callback
-        );
-      },
-      function (callback) {
-        brandCreate(
           "Volkswagen",
           "Volkswagen, abbreviated as VW, is a German motor vehicle manufacturer headquartered in Wolfsburg, Lower Saxony, Germany. ",
           callback
@@ -430,76 +416,46 @@ function createCarManufacturerBodyBrand(cb) {
   );
 }
 
-function createBooks(cb) {
+function createCars(cb) {
   async.parallel(
     [
+      // Name, model,  year, description, price, manufacturer, car_body, brand
       function (callback) {
-        bookCreate(
-          "The Name of the Wind (The Kingkiller Chronicle, #1)",
-          "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
-          "9781473211896",
-          authors[0],
-          [genres[0]],
+        carCreate(
+          "Volkswagen",
+          "Golf R",
+          "2023",
+          "Redesigned version of the popular Volkswagen Golf from 2022",
+          45090,
+          manufacturers[2],
+          carBodies[0],
+          brands[18],
           callback
         );
       },
       function (callback) {
-        bookCreate(
-          "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-          "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
-          "9788401352836",
-          authors[0],
-          [genres[0]],
+        carCreate(
+          "Acura",
+          "Integra",
+          "2023",
+          "An upscale version of the famour Honda Civic",
+          35800,
+          manufacturers[0],
+          carBodies[7],
+          brands[2],
           callback
         );
       },
       function (callback) {
-        bookCreate(
-          "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-          "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
-          "9780756411336",
-          authors[0],
-          [genres[0]],
-          callback
-        );
-      },
-      function (callback) {
-        bookCreate(
-          "Apes and Angels",
-          "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
-          "9780765379528",
-          authors[1],
-          [genres[1]],
-          callback
-        );
-      },
-      function (callback) {
-        bookCreate(
-          "Death Wave",
-          "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
-          "9780765379504",
-          authors[1],
-          [genres[1]],
-          callback
-        );
-      },
-      function (callback) {
-        bookCreate(
-          "Test Book 1",
-          "Summary of test book 1",
-          "ISBN111111",
-          authors[4],
-          [genres[0], genres[1]],
-          callback
-        );
-      },
-      function (callback) {
-        bookCreate(
-          "Test Book 2",
-          "Summary of test book 2",
-          "ISBN222222",
-          authors[4],
-          false,
+        carCreate(
+          "Kia",
+          "Sportage",
+          "2023",
+          "Redesigned version of the famour SUV",
+          30800,
+          manufacturers[8],
+          carBodies[10],
+          brands[4],
           callback
         );
       },
