22457cc874a5c17a4f60d7ef506c836ea520ce5a
budy6991 <alvmenfer@gmail.com>
2022-12-15 20:08:22 +0100

    Created all controllers, filled manufacturer controller out

---
+++ controllers/manufacturerController.js
@@ -0,0 +1,33 @@
+const Manufacturer = require("../models/manufacturer");
+
+exports.manufacturer_list = (req, res) => {
+  res.send("Not implemented: Manufacturer List");
+};
+
+exports.manufacturer_detail = (req, res) => {
+  res.send(`Not implemented: Manufacturer Detail: ${req.params.id}`);
+};
+
+exports.manufacturer_create_get = (req, res) => {
+  res.send("Not implemented: Manufacturer Create GET");
+};
+
+exports.manufacturer_create_post = (req, res) => {
+  res.send("Not implemented: Manufacturer Create POST");
+};
+
+exports.manufacturer_delete_get = (req, res) => {
+  res.send("Not implemented: Manufacturer Delete Get");
+};
+
+exports.manufacturer_delete_post = (req, res) => {
+  res.send("Not implemented: Manufacturer Delete POST");
+};
+
+exports.manufacturer_update_get = (req, res) => {
+  res.send("Not implemented: Manufacturer Update GET");
+};
+
+exports.manufacturer_update_post = (req, res) => {
+  res.send("Not implemented: Manufacturer Update POST");
+};
