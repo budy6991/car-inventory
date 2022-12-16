37f966a58dca7832f985c36c3cd07ce0fc021898
budy6991 <alvmenfer@gmail.com>
2022-12-16 19:07:13 +0100

    Added brand controller

---
+++ controllers/brandController.js
@@ -0,0 +1,33 @@
+const Brand = require("../models/brand");
+
+exports.brand_list = (req, res) => {
+  res.send("Not implemented: Brand List");
+};
+
+exports.brand_detail = (req, res) => {
+  res.send(`Not implemented: Brand Detail: ${req.params.id}`);
+};
+
+exports.brand_create_get = (req, res) => {
+  res.send("Not implemented Brand Create GET");
+};
+
+exports.brand_create_post = (req, res) => {
+  res.send("Not implemented Brand Create POST");
+};
+
+exports.brand_delete_get = (req, res) => {
+  res.send("Not implemented Brand delete GET");
+};
+
+exports.brand_delete_post = (req, res) => {
+  res.send("Not implemented Brand delete POST");
+};
+
+exports.brand_update_get = (req, res) => {
+  res.send("Not implemented Brand Update GET");
+};
+
+exports.brand_update_post = (req, res) => {
+  res.send("Not implemented Brand Update POST");
+};
