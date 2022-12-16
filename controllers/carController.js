39b724000463413bb8664abcff947e6b49d77afa
budy6991 <alvmenfer@gmail.com>
2022-12-16 19:13:47 +0100

    Added car controller

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,1 +1,37 @@
 const Car = require("../models/car");
+
+exports.index = (req, res) => {
+  res.send("Not implemented: Site Home Page");
+};
+
+exports.car_list = (req, res) => {
+  res.send("Not implemented: Car List");
+};
+
+exports.car_detail = (req, res) => {
+  res.send(`Not implemented Car Detail: ${req.params.id}`);
+};
+
+exports.car_create_get = (req, res) => {
+  res.send("Not implemented Car Create GET");
+};
+
+exports.car_create_post = (req, res) => {
+  res.send("Not implemented Car Create POST");
+};
+
+exports.car_delete_get = (req, res) => {
+  res.send("Not implemented Car Delete GET");
+};
+
+exports.car_delete_post = (req, res) => {
+  res.send("Not implemented Car Delete POST");
+};
+
+exports.car_update_get = (req, res) => {
+  res.send("Not implemented Car Update GET");
+};
+
+exports.car_update_post = (req, res) => {
+  res.send("Not implemented Car Update POST");
+};
