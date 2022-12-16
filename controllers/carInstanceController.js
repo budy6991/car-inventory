b645aa044e4ecf0f2f2d647cacae7dd97fdad8a0
budy6991 <alvmenfer@gmail.com>
2022-12-16 19:19:14 +0100

    Added controller for carInstance

---
+++ controllers/carInstanceController.js
@@ -0,0 +1,33 @@
+const CarInstance = require("../models/carInstance");
+
+exports.carinstance_list = (req, res) => {
+  res.send("Not implemented Car Instance Lists");
+};
+
+exports.caristance_detail = (req, res) => {
+  res.send(`Not implemented Car Instance Detail: ${req.params.id}`);
+};
+
+exports.carinstance_create_get = (req, res) => {
+  res.send("Not implemented CarInstance Create GET");
+};
+
+exports.carinstance_create_post = (req, res) => {
+  res.send("Not implemented CarInstance Create POST");
+};
+
+exports.carinstance_delete_get = (req, res) => {
+  res.send("Not implemented CarInstance delete GET");
+};
+
+exports.carinstance_delete_post = (req, res) => {
+  res.send("Not implemented CarInstance delete POST");
+};
+
+exports.carinstance_update_get = (req, res) => {
+  res.send("Not implemented CarInstance update GET");
+};
+
+exports.carinstance_update_post = (req, res) => {
+  res.send("Not implemented CarInstance update GET");
+};
