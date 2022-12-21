b442bf81b40edf9c4b65e14e4942797664609d1e
budy6991 <alvmenfer@gmail.com>
2022-12-21 01:53:26 +0100

    Fixed issue with brand url and added manufacturer detail view

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,6 +1,6 @@
 const Manufacturer = require("../models/manufacturer");
 const Car = require("../models/car");
-const Brand = require("../models/car");
+const Brand = require("../models/brand");
 const async = require("async");
 
 exports.manufacturer_list = (req, res, next) => {
@@ -18,11 +18,38 @@ exports.manufacturer_list = (req, res, next) => {
 };
 
 exports.manufacturer_detail = (req, res, next) => {
-  async.parallel({
-    manufacturer(callback) {
-      Manufacturer.findById(req.param.id).exec(callback);
+  async.parallel(
+    {
+      manufacturer(callback) {
+        Manufacturer.findById(req.params.id).exec(callback);
+      },
+      manufacturer_brands(callback) {
+        Brand.find({ manufacturer: req.params.id }).exec(callback);
+      },
+      manufacturer_cars(callback) {
+        Car.find({ manufacturer: req.params.id })
+          .populate("brand")
+          .exec(callback);
+      },
     },
-  });
+    (err, results) => {
+      console.log(results);
+      if (err) {
+        return next(err);
+      }
+      if (results.manufacturer == null) {
+        const err = new Error("Manufacturer not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("manufacturer_detail", {
+        title: "Manufacturer Detail",
+        manufacturer: results.manufacturer,
+        brands: results.manufacturer_brands,
+        cars: results.manufacturer_cars,
+      });
+    }
+  );
 };
 
 exports.manufacturer_create_get = (req, res) => {
